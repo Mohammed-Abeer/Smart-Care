144317b8aee625605298fa7917489ae8c82f21b8
abeer-hizucc <abeerrahman.24csedu.052@gmail.com>
2021-06-01 01:01:17 +0600

    3rd commit: added ManagerSignIn/Up,Customer Sign in

---
+++ app/src/main/java/com/example/smartcare/ManagerSignUp.java
@@ -0,0 +1,14 @@
+package com.example.smartcare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ManagerSignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manager_sign_up);
+    }
+}
