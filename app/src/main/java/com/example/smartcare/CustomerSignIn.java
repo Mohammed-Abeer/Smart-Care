e9f4723dcb2316d8bd43a0ee8cd21061bfd7f526
abeer-hizucc <abeerrahman.24csedu.052@gmail.com>
2021-05-31 22:13:04 +0600

    2nd commit:Added Customer sign up option

---
+++ app/src/main/java/com/example/smartcare/CustomerSignIn.java
@@ -0,0 +1,14 @@
+package com.example.smartcare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CustomerSignIn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_sign_in);
+    }
+}
