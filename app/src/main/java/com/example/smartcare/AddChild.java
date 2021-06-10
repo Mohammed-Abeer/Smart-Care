ee3a0397ceace2064adaf52c37ef9965c5f3114d
zulker01 <zulker02@gmail.com>
2021-06-10 13:44:50 +0600

    add child successful, left add task only

--- app/src/main/java/com/example/smartcare/AddChild.java
+++ app/src/main/java/com/example/smartcare/AddChild.java
@@ -1,4 +1,26 @@
 package com.example.smartcare;
 
-public class AddChild {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AddChild extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_child);
+
+        Button addChild = findViewById(R.id.add);
+        addChild.setOnClickListener(new View.OnClickListener() {
+                                         @Override
+                                         public void onClick(View v) {
+                                             startActivity(new Intent(AddChild.this, Dashboard.class));
+                                         }
+                                     }
+        );
+    }
 }
--- app/src/main/java/com/example/smartcare/Dashboard.java
+++ app/src/main/java/com/example/smartcare/Dashboard.java
@@ -2,7 +2,10 @@ package com.example.smartcare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 // this is customer dashboard
 public class Dashboard extends AppCompatActivity {
@@ -11,5 +14,14 @@ public class Dashboard extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        Button sendChild = findViewById(R.id.sendChild);
+        sendChild.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Dashboard.this, AddChild.class));
+            }
+        }
+        );
     }
 }
