a42f8b052582c80e5694f851430e9dfa07fb60a1
zulker01 <zulker02@gmail.com>
2021-06-10 18:13:17 +0600

    errorless

---
+++ app/src/main/java/com/example/smartcare/task/DialogCloseListerner.java
@@ -0,0 +1,7 @@
+package com.example.smartcare.task;
+
+import android.content.DialogInterface;
+
+public interface DialogCloseListerner {
+    public void handleDialogClose(DialogInterface dialog);
+}
